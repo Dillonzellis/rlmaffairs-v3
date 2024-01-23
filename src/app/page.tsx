871f186319f8f4ab4c1d62de17ef13fa8cc7bbc4
dillonzellis <dillonzellis@gmail.com>
2024-01-23 15:09:10 -0500

    feat: rough hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Yeppers
+    <main className="">
+      <Image
+        src="/hero-placeholder.jpg"
+        alt=""
+        width={1280}
+        height={834}
+        className="h-screen"
+      />
+      <div className="absolute top-7 left-7 flex gap-4">
+        <Image src="/hamburger.svg" alt="" width={45} height={20} />
+        <div className="text-white text-lg">MENU</div>
+      </div>
+      <h1 className="absolute left-1/2 top-[10%] text-8xl text-white -translate-x-1/2">
+        RLM
+      </h1>
     </main>
   );
 }
