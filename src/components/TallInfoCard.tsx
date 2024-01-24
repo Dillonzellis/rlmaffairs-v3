beb6d2a218faf76b2bf585ffea328462d17057d2
dillonzellis <dillonzellis@gmail.com>
2024-01-24 09:55:57 -0500

    feat: outline of info card component

---
+++ src/components/TallInfoCard.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export const TallInfoCard = () => {
+  return (
+    <div>
+      <div>
+        <Image src="" alt="" />
+      </div>
+      <div>
+        <div>Venue & Event Space</div>
+        <div>Venue & Event Space</div>
+      </div>
+    </div>
+  );
+};
