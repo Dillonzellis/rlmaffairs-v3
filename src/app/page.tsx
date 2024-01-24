a4a4611c40e6df2e29b5b0a07a41e496d77185bd
dillonzellis <dillonzellis@gmail.com>
2024-01-24 09:51:47 -0500

    feat: add down arr to hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <Image
-        src="/hero-placeholder.jpg"
-        alt=""
-        width={1280}
-        height={834}
-        className="h-screen"
-      />
-      <div className="absolute top-7 left-7 flex gap-4">
-        <Image src="/hamburger.svg" alt="" width={45} height={20} />
-        <div className="text-white text-lg">MENU</div>
-      </div>
-      <h1 className="font-serif absolute left-1/2 top-[10%] text-8xl text-white -translate-x-1/2">
-        RLM
-      </h1>
-      <NavDesktop />
+      <section className="min-h-screen">
+        <TallInfoCard />
+      </section>
     </main>
   );
 }
