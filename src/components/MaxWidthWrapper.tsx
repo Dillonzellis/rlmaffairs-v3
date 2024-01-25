c7e14af3dc28094fccc86ac99f3b136f792bf9e6
dillonzellis <dillonzellis@gmail.com>
2024-01-25 13:20:28 -0500

    feat: modify MaxWidthWrapper to handle cva

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,20 +1,41 @@
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+
+// type MaxWidthWrapperVariants = "default" | "sm" | "md" | "lg" | "xl";
+
+type MaxWidthWrapperVariants = VariantProps<typeof MaxWidthWrapperVariants>;
+
+interface MaxWidthWrapperProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    MaxWidthWrapperVariants {
+  children: ReactNode;
+  className?: string;
+}
+
+const MaxWidthWrapperVariants = cva(
+  "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+  {
+    variants: {
+      cols: {
+        default: "",
+        twoCols: "grid grid-cols-2 gap-16",
+      },
+
+      defaultVariants: {
+        cols: "default",
+      },
+    },
+  },
+);
 
 const MaxWidthWrapper = ({
   className,
   children,
-}: {
-  className?: string;
-  children: ReactNode;
-}) => {
+  cols,
+}: MaxWidthWrapperProps) => {
   return (
-    <div
-      className={cn(
-        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
-        className,
-      )}
-    >
+    <div className={cn(MaxWidthWrapperVariants({ cols }), className)}>
       {children}
     </div>
   );
--- src/components/TallInfoCard.tsx
+++ src/components/TallInfoCard.tsx
@@ -1,12 +1,9 @@
 import Image from "next/image";
-import { Button } from "./ui/button";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { ContentCard } from "./ContentCard";
+import type { ContentCardProps } from "@/components/ContentCard";
 
-interface TallInfoCardProps {
-  title: string;
-  subtext: string;
-  buttonText: string;
-  buttonLink: string;
+interface TallInfoCardProps extends ContentCardProps {
   image: string;
   imageAlt: string;
 }
@@ -20,7 +17,7 @@ export const TallInfoCard = ({
   imageAlt,
 }: TallInfoCardProps) => {
   return (
-    <MaxWidthWrapper className="grid grid-cols-2 gap-16">
+    <MaxWidthWrapper cols="twoCols">
       <div className="flex justify-center p-8 bg-[#ECECEC] relative">
         <Image
           src="/paper-bg.jpg"
@@ -37,14 +34,12 @@ export const TallInfoCard = ({
           className="shadow-md shadow-muted-foreground z-10 hover:scale-[1.03] transition-all ease-in-out duration-700"
         />
       </div>
-      <div className="flex flex-col justify-center gap-1.5">
-        <div className="text-primary/40 text-sm">{subtext}</div>
-        <div className="text-5xl pb-2">{title}</div>
-        <a href={buttonLink} className="text-3xl flex gap-6">
-          <Image src="/right-arrow.svg" alt="" width={55} height={20} />
-          {buttonText}
-        </a>
-      </div>
+      <ContentCard
+        title={title}
+        subtext={subtext}
+        buttonText={buttonText}
+        buttonLink={buttonLink}
+      />
     </MaxWidthWrapper>
   );
 };
